8630aeec656e2d13cf3f1943d03906f62a4114e6
Deepakgupta2006 <dg7842661@gmail.com>
2026-08-22 00:45:28 +0530

    Upadate FibonacciNumber.java

--- 00_Loops/FibonacciNumber.java
+++ 00_Loops/FibonacciNumber.java
@@ -1,21 +1,12 @@
 class Solution {
     public int fib(int n) {
-
-        if (n <= 1) {
-            return n;
-        }
-
-        int first = 0;
-        int second = 1;
-
+        if (n <= 1) return n;
+        int a = 0, b = 1;
         for (int i = 2; i <= n; i++) {
-
-            int next = first + second;
-
-            first = second;
-            second = next;
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
-
-        return second;
+        return b;
     }
 }
