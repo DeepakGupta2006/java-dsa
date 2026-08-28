df92c9377578aa076c55b8725b1d6eaf81a18f3c
Deepakgupta2006 <dg7842661@gmail.com>
2026-08-28 06:08:16 +0530

    Remove ActivitySelection.java

---
+++ 12_Greedy/ActivitySelection.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ActivitySelection {
+
+    public int maxActivities(int[][] activities) {
+        Arrays.sort(activities, Comparator.comparingInt(a -> a[1]));
+
+        int count = 1;
+        int lastEnd = activities[0][1];
+
+        for (int i = 1; i < activities.length; i++) {
+            if (activities[i][0] >= lastEnd) {
+                count++;
+                lastEnd = activities[i][1];
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        ActivitySelection solution = new ActivitySelection();
+        int[][] activities = {{1, 3}, {2, 5}, {4, 6}, {6, 8}, {5, 9}};
+        System.out.println("Max activities: " + solution.maxActivities(activities));
+    }
+}
