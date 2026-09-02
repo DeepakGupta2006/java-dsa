8eca9f19fd6527fa29a54acee378e643d9de6720
Deepakgupta2006 <dg7842661@gmail.com>
2026-09-03 04:42:51 +0530

    Update TwoSum.java

--- 01_Arrays/TwoSum.java
+++ 01_Arrays/TwoSum.java
@@ -1,13 +1,15 @@
-public class TwoSum {
-    public static void main(String[] args) {
-        int[] arr = {2,6,5,8,11};
-        int target = 14;
-        for ( int i = 0; i < arr.length; i++ ) {
-            for ( int j = i + 1; j < arr.length; j++ ) {
-                if ( arr[i] + arr[j] == target ) {
-                    System.out.println("Pair found: " + arr[i] + ", " + arr[j]);
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
+        return new int[]{};
     }
 }
